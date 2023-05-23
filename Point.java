65206ee926a9c2ca0cfe0092e35c9b288da79541
Klahadore <stringycheese@protonmail.com>
2023-05-22 22:36:51 -0400

    added point class

---
+++ Point.java
@@ -0,0 +1,22 @@
+public class Point() {
+    long x;
+    long y;  
+
+    public Point(long x, long y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public long getX() {
+        return x;
+    }
+
+    public long getY() {
+        return y
+        
+        ;
+    }
+}
+
+
+
